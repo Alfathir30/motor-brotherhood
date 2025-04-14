1210fb5eafe1fa3858bee1307d6955f24c65556a
MarvelzId <nandien4409@gmail.com>
2025-04-14 15:57:50 +0700

    Update fitur baru atau perbaikan

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-function toggleMenu() {
-  const nav = document.querySelector('nav');
-  nav.classList.toggle('active');
-}
+// Ambil elemen yang diperlukan
+const mobileMenu = document.getElementById('mobile-menu');
+const navbar = document.querySelector('.navbar');
+
+// Toggle class untuk menampilkan/menyembunyikan navbar
+mobileMenu.addEventListener('click', () => {
+  navbar.classList.toggle('active');
+});
